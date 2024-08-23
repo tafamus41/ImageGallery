6e66a348f66f42067dc234833e7a153a96a90f89
asimov <seyranmustafa4@gmail.com>
2024-08-23 20:48:54 +0300

    first commit

---
+++ myapp/src/helper/data.js
@@ -0,0 +1,159 @@
+const data = [
+    {
+      id: 1,
+      name: "Ersin Destanoğlu",
+      img: "https://images.bjk.com.tr/images/players/ersin-destanoglu-2-2_229x270_000004.png",
+    },
+    {
+      id: 2,
+      name: "Mert Günok",
+      img: "https://images.bjk.com.tr/images/players/mert-gunok_229x270_000002.jpg",
+    },
+    {
+      id: 3,
+      name: "Göktuğ Baytekin",
+      img: "https://images.bjk.com.tr/images/players/goktug-baytekin_229x270_000002.png",
+    },
+    {
+      id: 4,
+      name: "Emir Yaşar",
+      img: "https://images.bjk.com.tr/images/players/emir-yasar-3_229x270.png",
+    },
+    {
+      id: 5,
+      name: "Arthur Masuaku",
+      img: "https://images.bjk.com.tr/images/players/arthur-masuaku_229x270_000002.png",
+    },
+    {
+      id: 6,
+      name: "Talha Sanuç",
+      img: "https://images.bjk.com.tr/images/players/tayyip-talha-sanuc_229x270_000001.png",
+    },
+    {
+      id: 7,
+      name: "Onur Bulut",
+      img: "https://images.bjk.com.tr/images/players/onur-bulut_229x270_000002.png",
+    },
+    {
+      id: 8,
+      name: "Omar Colley",
+      img: "https://images.bjk.com.tr/images/players/omar-colley_229x270_000002.png",
+    },
+    {
+      id: 9,
+      name: "Jonas Svensson",
+      img: "https://images.bjk.com.tr/images/players/jonas-svensson_229x270_000004.png",
+    },
+    {
+      id: 10,
+      name: "Arda Berk Özüarap",
+      img: "https://images.bjk.com.tr/images/players/arda-berk-ozuarap-3-2_229x270.png",
+    },
+    {
+      id: 11,
+      name: "Gabriel Paulista",
+      img: "https://images.bjk.com.tr/images/players/gabriel-paulista_229x270.png",
+    },
+    {
+      id: 12,
+      name: "Emirhan Topçu",
+      img: "https://images.bjk.com.tr/images/players/emirhan-topcu_229x270.jpg",
+    },
+    {
+      id: 13,
+      name: "Necip Uysal",
+      img: "https://images.bjk.com.tr/images/players/necip-uysal_229x270_000008.png",
+    },
+    {
+      id: 14,
+      name: "Salih Uçan",
+      img: "https://images.bjk.com.tr/images/players/salih-ucan_229x270_000002.png",
+    },
+    {
+      id: 15,
+      name: "Demir Ege Tıknaz",
+      img: "https://images.bjk.com.tr/images/players/demir-ege-tiknaz_229x270_000002.png",
+    },
+    {
+      id: 16,
+      name: "Fahri Kerem Ay",
+      img: "https://images.bjk.com.tr/images/players/fahri-kerem-ay-3_229x270_000001.jpg",
+    },
+    {
+      id: 17,
+      name: "Yakup Arda Kılıç",
+      img: "https://images.bjk.com.tr/images/players/yakup-arda-kilic-2_229x270.png",
+    },
+    {
+      id: 18,
+      name: "Gedson Carvalho Fernandes",
+      img: "https://images.bjk.com.tr/images/players/gedson-carvalho-fernandes_229x270_000002.png",
+    },
+    {
+      id: 19,
+      name: "Serkan Emrecan Terzi",
+      img: "https://images.bjk.com.tr/images/players/serkan-emrecan-terzi-2_229x270.png",
+    },
+    {
+      id: 20,
+      name: "Jean Onana",
+      img: "https://images.bjk.com.tr/images/players/jean-onana_229x270_000001.png",
+    },
+    {
+      id: 21,
+      name: "Milot Rashica",
+      img: "https://images.bjk.com.tr/images/players/milot-rashica_229x270_000001.png",
+    },
+    {
+      id: 22,
+      name: "Bahtiyar Zainutdinov",
+      img: "https://images.bjk.com.tr/images/players/bakhtiyor-zaynutdinov_229x270_000001.png",
+    },
+    {
+      id: 23,
+      name: "Ernest Muçi",
+      img: "https://images.bjk.com.tr/images/players/ernest-muci_229x270.png",
+    },
+    {
+      id: 24,
+      name: "Al Musrati",
+      img: "https://images.bjk.com.tr/images/players/almoatasembellah-ali-mohamed-al-musrati_229x270.png",
+    },
+    {
+      id: 25,
+      name: "Rafa Silva",
+      img: "https://images.bjk.com.tr/images/players/rafael-silva_229x270.png",
+    },
+    {
+      id: 26,
+      name: "Can Keleş",
+      img: "https://images.bjk.com.tr/images/players/can-keles_229x270.png",
+    },
+    {
+      id: 27,
+      name: "Cher Ndour",
+      img: "https://images.bjk.com.tr/images/players/cher-ndour_229x270.jpg",
+    },
+    {
+      id: 28,
+      name: "Vincent Aboubakar",
+      img: "https://images.bjk.com.tr/images/players/vincent-aboubakar_229x270_000003.png",
+    },
+    {
+      id: 29,
+      name: "Semih Kılıçsoy",
+      img: "https://images.bjk.com.tr/images/players/semih-kilicsoy-2-2_229x270_000001.png",
+    },
+    {
+      id: 30,
+      name: "Mustafa Erhan Hekimoğlu",
+      img: "https://images.bjk.com.tr/images/players/mustafa-erhan-hekimoglu-2_229x270.png",
+    },
+    {
+      id: 31,
+      name: "Ciro Immobile",
+      img: "https://images.bjk.com.tr/images/players/ciro-immobile_229x270.png",
+    },
+  ];
+  
+  export default data;
